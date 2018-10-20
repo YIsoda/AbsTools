5fefa18aab22bb511646a146d568abedbba83d99
Y.Isoda <y.isoda.3953@gmail.com>
2018-10-20 22:51:33 +0900

    add Prism package and created a file contains Model

---
+++ AbsToolGui/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace AbsToolGui
+{
+
+}
+
+namespace MVVMModel
+{
+	using Prism.Mvvm;
+
+	class Model : BindableBase
+	{
+		public ObservableCollection<DataFile> DataFileList { get; private set; }
+
+		public void ExportFiles() { }
+	}
+
+	public class DataFile : BindableBase
+	{
+		private FileInfo _fileInfo;
+
+		private bool _isValidData;
+
+		public FileInfo FileInfo { get => _fileInfo; set => SetProperty(ref _fileInfo, value); }
+
+		public bool IsValidData { get => _isValidData; set => SetProperty(ref _isValidData, value); }
+	}
+}
+
+namespace ViewModel
+{
+	public class ViewModel
+	{
+		private MVVMModel.Model model = new MVVMModel.Model();
+	}
+}
